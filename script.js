762a22ea953cafc0a6821b8f4fe705e0a24583ab
Yukta Vaidya <127371912+Yukta2108@users.noreply.github.com>
2024-06-03 12:05:49 +0530

    NEW PORTFOLIO

---
+++ script.js
@@ -0,0 +1,64 @@
+// -------------------TOGGLE NAVBAR-------------
+
+let menuIcon = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('fa-xmark');
+  navbar.classList.toggle('active');
+
+}
+
+
+
+let section = document.querySelectorAll('section');
+let navlink = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+        navlink.forEach(link =>{
+          link.classList.remove('active');
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        })
+    };
+  })
+// --------------------------------------STICKY NAVBAR----------------
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+  
+    // ---------------------remove navbar when click----------
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+// ---------------------SCROLL -------------------
+
+ScrollReveal ({
+  // reset:true,
+  distance: '80px',
+  duration:2000,
+  delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'bottom'});
+
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+
+// ---------------------------TYPED TEXT-----------------
+
+
+var typed = new Typed('.type-text', {
+  strings: ['<i>Frontend</i> Developer.', ' Data Analyst.' , 'Content Writer'],
+  typeSpeed: 100,
+  backspeed: 100,
+  loop: true
+});
